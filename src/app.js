4eddbfa46a92322eea7eca9709bae21afc32c965
Naman Nayal <nayalnaman@gmail.com>
2025-02-04 01:41:26 +0530

    exploring validators

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 //handler function as async
+//to create a user in the db
 app.post("/signup",async(req,res)=>{
     // const userObj = {
     //     firstName:"Naman",
@@ -25,6 +26,69 @@ app.post("/signup",async(req,res)=>{
     res.send("User created successfully");
     }catch(err){
     res.status(400).send("Error creating user");
+    }
+})
+//to get user from the db
+app.get("/user",async(req, res) => {
+    const userEmail = req.body.emailId;
+    if(!userEmail){
+        return res.status(400).send("Email is required");
+    }
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        if(!user){
+            return res.status(404).send("User not found");
+        }else{
+            res.send(user);
+        }
+       
+    }catch(e){
+        res.status(400).send("Error fetching user");
+    }
+})
+//Feed API  GET feed  get all the users from the db
+app.get('/feed',async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(500).send("Server Error");
+    }
+
+})
+//to delete user from the database 
+app.delete('/user', async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(500).send("Error deleting user");
+    }
+})
+//to update user from the database
+app.patch('/user/:userId', async(req,res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+   
+    try{
+        const ALLOWED_UPDATE = ["photoUrl","about","gender","skills"];
+        const isUpdateAlllowed = Object.keys(data).every((k)=> ALLOWED_UPDATE.includes(k));
+        if(!isUpdateAlllowed){
+            throw new Error("Invalid updates");
+        }
+
+        if(data?.skills.length > 10){
+            throw new Error("Skills limit exceeded");
+        }
+
+        await User.findByIdAndUpdate({_id: userId},data,{
+            returnDocument:"after",
+            runValidators:true,
+        });
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(500).send("Error updating user");
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,65 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const useSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName:{
         type:String,
     },
     emailId:{
         type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email "+value);
+        }
     },
     password:{
         type:String,
+        required:true,
+        validator(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Use Strong Password "+value);
+        }
     },
     age:{
         type:Number,
+        min:9,
     },
     gender:{
         type:String,
+
+        validate(value){
+        if(!["male", "female", "other"].includes(value)){
+            throw new Error("Invalid gender");
+        }
+    },
+
+    },
+    photoUrl:{
+        type:String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+        validator(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL "+value);
+        }
+    },
+    about:{
+        type:String,
+        default:"Hey there! I am using DevTinder",
+    },
+    skills:{
+        type:[String],
     }
+},{
+    timestamps:true,
 });
 
 
